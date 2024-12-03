a73e1804a3e74cb94e2690f68410a59cb6bbb7fd
luciameneghin <lucia.meneghin00@gmail.com>
2024-12-03 15:29:22 +0100

    feat: push 10 emails into an array

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail'
 
+// array vuoto per le 10 email random
+let emailArray = [];
+let counter = 0;
+
 // funzione per una email
-axios.get(endpoint)
-  .then(response => {
-    console.log(response.data.response);
-  })
-  .catch(err => {
-    console.log(`Errore nella richiesta dell\'email:`, err);
-  })
+function email() {
+  axios.get(endpoint)
+    .then(response => {
+      emailArray.push(response.data.response)
+      counter++;
+      if (counter === 10) {
+        console.log(emailArray);
+      }
+    })
+    .catch(err => {
+      console.log(`Errore nella richiesta dell\'email:`, err);
+    });
+}
+
+// chiamata alla funzione per ottenere la risposta HTTP
+for (let i = 0; i < 10; i++) {
+  email()
+}
 
